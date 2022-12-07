9b7889ff87cdff8c3bfe847aa653dfe3487ea383
Jogira <46968282+Jogira@users.noreply.github.com>
2022-12-07 03:23:38 +0000

    Step done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 function App() {
-  return (
+const name = "StarGazers"
+const css = {color:"SteelBlue"}
+return (
+  <div className="conainer">    
+    <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
+      <img src="images/group.svg" alt="Stargazers Group" />
+      <h1>Meet the <i style={css}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+    <button className="outline" onClick={()=>alert("Hi there")}>Click Me</button>
     </hgroup>
+    </article>
+  </div>
+
   )
 }
 export default App
